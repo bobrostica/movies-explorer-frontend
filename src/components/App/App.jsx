6f5c74245f6dd5f8393ec0f4512f061dc0fe56f2
Sergey Berdnikov <sega12b@yandex.ru>
2023-08-14 20:44:14 +0500

    feat: make basis of the main page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import Main from '../Main/Main';
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route>
+          <Route path="/" element={<Main />} />
+          <Route path="/movies" />
+          <Route path="/saved-movies" />
+          <Route path="/profile" />
+        </Route>
+        <Route path="/signin" />
+        <Route path="/signup" />
+        <Route path="*" />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
