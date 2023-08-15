064339dee953af697cd31c971c54c534268839c8
Sergey Berdnikov <sega12b@yandex.ru>
2023-08-15 13:21:52 +0500

    feat: make promo section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Promo from '../../ui/Promo/Promo';
 
 const Main = () => (
   <>
     <Outlet />
+    <Promo />
   </>
 );
 
---
+++ src/ui/Promo/Promo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Promo.css';
+import PromoLogo from '../PromoLogo/PromoLogo';
+
+const Promo = () => (
+  <section className="promo">
+    <PromoLogo />
+    <div className="promo__content">
+      <h1 className="promo__title">
+        Учебный проект студента факультета Веб-разработки.
+      </h1>
+    </div>
+  </section>
+);
+
+export default Promo;
