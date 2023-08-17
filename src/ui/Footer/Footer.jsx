dd8aabd6f166e68563d85cc30786e621c08bd369
Sergey Berdnikov <sega12b@yandex.ru>
2023-08-17 16:33:52 +0500

    refcat: footer adaptive

--- src/ui/Footer/Footer.jsx
+++ src/ui/Footer/Footer.jsx
@@ -3,42 +3,41 @@ import React from 'react';
 import './Footer.css';
 import TextLink from '../TextLink/TextLink';
 import FlexList from '../FlexList/FlexList';
+import MainSection from '../MainSection/MainSection';
 
 const Footer = ({ githubLink }) => (
-  <section className="footer">
-    <div className="footer__content">
-      <h2 className="footer__title">
-        Учебный проект Яндекс.Практикум х BeatFilm.
-      </h2>
-      <div className="footer__container">
-        <p className="footer__copyright">© 2023</p>
-        <nav>
-          <FlexList className="footer__nav-list">
-            <li>
-              <TextLink
-                href="https://practicum.yandex.ru"
-                className="student-section__portfolio-link"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Яндекс.Практикум
-              </TextLink>
-            </li>
-            <li>
-              <TextLink
-                href={githubLink}
-                className="student-section__portfolio-link"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Github
-              </TextLink>
-            </li>
-          </FlexList>
-        </nav>
-      </div>
+  <MainSection className="footer" paddingSize="s">
+    <h2 className="footer__title">
+      Учебный проект Яндекс.Практикум х BeatFilm.
+    </h2>
+    <div className="footer__container">
+      <p className="footer__copyright">© 2023</p>
+      <nav>
+        <FlexList className="footer__nav-list">
+          <li>
+            <TextLink
+              href="https://practicum.yandex.ru"
+              className="footer__link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Яндекс.Практикум
+            </TextLink>
+          </li>
+          <li>
+            <TextLink
+              href={githubLink}
+              className="footer__link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </TextLink>
+          </li>
+        </FlexList>
+      </nav>
     </div>
-  </section>
+  </MainSection>
 );
 
 export default Footer;
